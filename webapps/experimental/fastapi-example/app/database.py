ae9ee6931a0b7ac1e167d8ed93de3fe610f12f56
mandarup <mandarupadhye@gmail.com>
2019-10-02 15:37:58 -0400

    add basic fastapi example

---
+++ webapps/experimental/fastapi-example/app/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///../test.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+
+## NOTE:
+## connect_args={"check_same_thread": False}
+## is needed only for SQLite. It's not needed for other databases.
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
